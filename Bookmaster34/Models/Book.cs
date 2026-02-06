3b56211b8d7fe1670b1fc31af7276b80dd120b35
vboxuser <vboxuser@WIN10>
2026-02-06 10:39:17 +0300

    Сгенерировали модели таблиц БД

--- Bookmaster34/Models/Book.cs
+++ Bookmaster34/Models/Book.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Bookmaster34.Models
+namespace Bookmaster34.Models;
+
+public partial class Book
 {
-    class Book
-    {
-    }
+    public string Id { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public string? Subtitle { get; set; }
+
+    public DateOnly? FirstPublishDate { get; set; }
+
+    public string? Description { get; set; }
+
+    public virtual ICollection<BookAuthor> BookAuthors { get; set; } = new List<BookAuthor>();
+
+    public virtual ICollection<BookCover> BookCovers { get; set; } = new List<BookCover>();
+
+    public virtual ICollection<BookSubject> BookSubjects { get; set; } = new List<BookSubject>();
+
+    public virtual ICollection<Circulation> Circulations { get; set; } = new List<Circulation>();
 }
