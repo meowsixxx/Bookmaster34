1c9d290be266dde42a4c08fdbb89b85c55066cf4
vboxuser <vboxuser@WIN10>
2026-02-20 10:35:18 +0300

    Добавили навигацию

--- Bookmaster34/View/Windows/MainWindow.xaml.cs
+++ Bookmaster34/View/Windows/MainWindow.xaml.cs
@@ -21,6 +21,7 @@ namespace Bookmaster34
         public MainWindow()
         {
             InitializeComponent();
+            MainFrame.Navigate(new BrowseCatalogPage());    
         }
 
         private void LoginMi_Click(object sender, RoutedEventArgs e)
